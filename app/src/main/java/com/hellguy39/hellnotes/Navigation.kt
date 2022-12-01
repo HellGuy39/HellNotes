6596f31f92f42071f0dff9fef4fade865ca77354
HellGuy39 <alexey2005yt@gmail.com>
2022-12-01 10:00:06 +0300

    Setup animations NoteList -> NoteDetail & back

--- app/src/main/java/com/hellguy39/hellnotes/Navigation.kt
+++ app/src/main/java/com/hellguy39/hellnotes/Navigation.kt
@@ -1,18 +1,22 @@
 package com.hellguy39.hellnotes
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.hellguy39.hellnotes.notes.util.noteDetailScreen
 import com.hellguy39.hellnotes.notes.util.noteListNavigationRoute
 import com.hellguy39.hellnotes.notes.util.noteListScreen
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation() {
 
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
 
-    NavHost(
+    AnimatedNavHost(
         navController = navController,
         startDestination = noteListNavigationRoute
     ) {
--- feature/notes/src/main/java/com/hellguy39/hellnotes/notes/util/Navigation.kt
+++ feature/notes/src/main/java/com/hellguy39/hellnotes/notes/util/Navigation.kt
@@ -1,7 +1,9 @@
 package com.hellguy39.hellnotes.notes.util
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.navigation.*
-import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.composable
 import com.hellguy39.hellnotes.notes.detail.NoteDetailRoute
 import com.hellguy39.hellnotes.notes.list.NoteListRoute
 
@@ -26,14 +28,30 @@ fun NavController.navigateToListNote(navOptions: NavOptions? = null) {
     navigate(noteDetailNavigationRoute, navOptions)
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.noteListScreen(
     navController: NavController
 ) {
-    composable(route = noteListNavigationRoute) {
+    composable(
+        route = noteListNavigationRoute,
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -300 },
+                animationSpec = tween(300)
+            ) + fadeOut(animationSpec = tween(300))
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -300 },
+                animationSpec = tween(300)
+            ) + fadeIn(animationSpec = tween(300))
+        }
+    ) {
         NoteListRoute(navController)
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.noteDetailScreen(
     navController: NavController
 ) {
@@ -43,7 +61,19 @@ fun NavGraphBuilder.noteDetailScreen(
             navArgument(name = KEY_NOTE_ID) {
                 type = NavType.IntType
             }
-        )
+        ),
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { 300 },
+                animationSpec = tween(300)
+            ) + fadeIn(animationSpec = tween(300))
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { 300 },
+                animationSpec = tween(300)
+            ) + fadeOut(animationSpec = tween(300))
+        }
     ) {
         val noteId = it.arguments?.getInt(KEY_NOTE_ID)
 
