4540dc409a0274c69002392d5c75a6d37e4b49f6
Aleksey Gadzhiev <75219002+HellGuy39@users.noreply.github.com>
2025-07-10 20:01:18 +0300

    Release 1.2.3 (#16)
    * Update target sdk
    
    * Update app icon

--- build-logic/convention/src/main/kotlin/Configuration.kt
+++ build-logic/convention/src/main/kotlin/Configuration.kt
@@ -1,11 +1,11 @@
 object Configuration {
 
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 29 // Android 10
-    const val targetSdk = 34
+    const val targetSdk = 35
 
-    const val versionCode = 11
-    const val versionName = "1.2.2" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+    const val versionCode = 12
+    const val versionName = "1.2.3" // X.Y.Z; X = Major, Y = minor, Z = Patch level
     const val applicationId = "com.hellguy39.hellnotes"
 
     const val composeCompiler = "1.5.9"
